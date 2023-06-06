5560dd2e5404bcd24c15b64c8d735a672128e273
JanGamesHD <64363088+JanGamesHD@users.noreply.github.com>
2023-06-06 20:24:11 +0200

    Add files via upload

---
+++ 1/7/test.bat
@@ -0,0 +1,74 @@
+@echo off
+:reload
+cls
+echo NOTE: This program is not affiliated with Mojang or Microsoft.
+echo Current Directory: %cd%
+echo Application Directory: %appdir%
+echo Minecraft workDir: %appdir%\MCInstall
+echo Porti WorkDir: %portiworkdir%
+if exist %appdir%\Minecraft.exe echo 1) Start Minecraft Launcher
+if not exist %appdir%\Minecraft.exe echo 1) Download Minecraft Launcher
+echo 2) Terminate Minecraft Launcher +javaw (game)
+echo 3) Remove Minecraft Launcher
+echo 4) Exit
+set /p opt=Opt: 
+if %opt%==1 goto checkmclauncher
+if %opt%==2 goto confirmation
+if %opt%==3 goto confirmation2
+if %opt%==4 exit
+echo The selected option was not found.
+pause
+goto reload
+rem powershell gc banana2.txt -Wait
+
+:checkmclauncher
+cls
+echo Checking ...
+if not exist %appdir%\Minecraft.exe goto downloadmclauncher
+goto launch
+
+:downloadmclauncher
+rem https://launcher.mojang.com/download/Minecraft.exe
+%portiworkdir%\wget.exe https://launcher.mojang.com/download/Minecraft.exe -O %appdir%\Minecraft.exe
+goto reload
+
+:confirmation
+cls
+echo Are you sure you want to close off the Minecraft Launcher and java?
+echo If you have any running worlds open, they might get corrupted.
+echo Do you still want to contiune? (y/n)
+set /p opt=Opt: 
+if %opt%==y goto killmc
+if %opt%==n goto reload
+goto confirmation
+
+:killmc
+taskkill /F /IM Minecraft.exe /T
+taskkill /F /IM java.exe /T
+taskkill /F /IM javaw.exe /T
+ech Done!
+pause
+goto reload
+
+:confirmation2
+cls
+echo Do you want to delete Minecraft.exe ? (y/n)
+echo NOTE: Make sure to close off the Minecraft Launcher before you proceed.
+set /p opt=Opt: 
+if %opt%==y goto delmcexe
+if %opt%==n goto reload
+goto confirmation2
+
+:delmcexe
+del %appdir%\Minecraft.exe /Q /F
+echo Done!
+pause
+goto reload
+
+:launch
+cls
+echo Starting Minecraft launcher...
+echo Executing: %appdir%\Minecraft.exe --workDir %cd%\%appdir%
+start cmd /c "%appdir%\Minecraft.exe --workDir %cd%\%appdir%"
+pause
+goto reload
