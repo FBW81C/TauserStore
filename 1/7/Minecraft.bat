d1b5e2de961c918bd6cdc6e91b31e33a7b96a3d5
JanGamesHD <64363088+JanGamesHD@users.noreply.github.com>
2023-06-23 19:15:18 +0200

    Update Minecraft.bat

--- 1/7/Minecraft.bat
+++ 1/7/Minecraft.bat
@@ -5,9 +5,13 @@ echo NOTE: This program is not affiliated with Mojang or Microsoft.
 echo Current Directory: %cd%
 echo Application Directory: %appdir%
 echo Minecraft workDir: %appdir%\MCInstall
+set exist=0
 echo Porti WorkDir: %portiworkdir%
 if exist %appdir%\Minecraft.exe echo 1) Start Minecraft Launcher
-if not exist %appdir%\Minecraft.exe echo 1) Download Minecraft Launcher
+if exist %appdir%\olauncher.jar echo 1) Start Minecraft Launcher (UNOFFICIAL)
+if exist %appdir%\Minecraft.exe set exist=1
+if exist %appdir%\olauncher.jar set exist=1
+if %exist%==0 echo 1) Download Minecraft Launcher
 echo 2) Terminate Minecraft Launcher +javaw (game)
 echo 3) Remove Minecraft Launcher
 echo 4) Exit
@@ -24,12 +28,14 @@ rem powershell gc banana2.txt -Wait
 :checkmclauncher
 cls
 echo Checking ...
-if not exist %appdir%\Minecraft.exe goto downloadmclauncher
-goto launch
+rem if not exist %appdir%\Minecraft.exe goto downloadmclauncher
+if exist %appdir%\Minecraft.exe goto launch
+if exist %appdir%\olauncher.jar goto launchunofficial
+goto whichlauncher
 
 :downloadmclauncher
 rem https://launcher.mojang.com/download/Minecraft.exe
-%portiworkdir%\wget.exe https://launcher.mojang.com/download/Minecraft.exe -O %appdir%\Minecraft.exe
+%portiworkdir%\wget.exe https://launcher.mojang.com/download/Minecraft.exe -O "%appdir%\Minecraft.exe"
 goto reload
 
 :confirmation
@@ -52,7 +58,7 @@ goto reload
 
 :confirmation2
 cls
-echo Do you want to delete Minecraft.exe ? (y/n)
+echo Do you want to delete Minecraft.exe and / or olauncher.jar ? (y/n)
 echo NOTE: Make sure to close off the Minecraft Launcher before you proceed.
 set /p opt=Opt: 
 if %opt%==y goto delmcexe
@@ -61,6 +67,7 @@ goto confirmation2
 
 :delmcexe
 del %appdir%\Minecraft.exe /Q /F
+del %appdir%\olauncher.jar /Q /F
 echo Done!
 pause
 goto reload
@@ -72,3 +79,42 @@ echo Executing: %appdir%\Minecraft.exe --workDir %cd%\%appdir%
 start cmd /c "%appdir%\Minecraft.exe --workDir %cd%\%appdir%"
 pause
 goto reload
+
+:whichlauncher
+cls
+echo Choose an Minecraft Launcher.
+echo 1) Official Minecraft Launcher from Mojang
+echo 2) Olauncher from github (https://github.com/olauncher/olauncher) (Works better on older PCs and USB Devices, but requires java)
+set /p opt=Opt: 
+if %opt%==1 goto downloadmclauncher
+if %opt%==2 goto downloadolauncher
+goto whichlauncher
+
+:downloadolauncher
+cls
+echo Checking for Java...
+if not exist "%portiworkdir%\Java\jdk-17.0.7+7\bin\java.exe" goto javanotfound
+goto startdownloadolauncher
+
+:javanotfound
+cls
+echo You need to install Java to use Olauncher.
+echo Open the Tauser Store from Porti, select bat software and Download OpenJDK / Java Microsoft Build (option 8)
+echo You can also install a different Java version into the "%portiworkdir%\Java\" directory.
+echo Make sure the java executable is stored in these sub-folders: "%portiworkdir%\Java\jdk-17.0.7+7\bin\java.exe"
+pause
+goto reload
+
+:startdownloadolauncher
+cls
+echo Java has been detected in "%portiworkdir%\Java\jdk-17.0.7+7\bin\java.exe"
+echo Downloading olauncher from github...
+"%portiworkdir%\wget.exe" https://github.com/olauncher/olauncher/releases/download/v1.7.2_08/olauncher-1.7.2_08-redist.jar -O "%cd%\%appdir%\olauncher.jar"
+echo Done!
+pause
+goto reload
+
+:launchunofficial
+cls
+echo Java: launching %cd%\%appdir%\olauncher.jar
+"%portiworkdir%\Java\jdk-17.0.7+7\bin\java.exe" -jar "%cd%\%appdir%\olauncher.jar" --workDir "%cd%\%appdir%"
